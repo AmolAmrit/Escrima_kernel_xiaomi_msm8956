f1be0bed3164fb534deb90de8fb1432895e18dd7
AmolAmrit <amol.amrit03@outlook.com>
2017-10-16 23:38:15 +0530

    Be Back now
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -3829,7 +3829,8 @@ static void status_change_work(struct work_struct *work)
 	if (chip->status == POWER_SUPPLY_STATUS_FULL ||
 			chip->status == POWER_SUPPLY_STATUS_CHARGING) {
 		is_charging = true;
-
+                if (!chip->vbat_low_irq_enabled &&
+                                !chip->use_vbat_low_empty_soc) {
 		if (!chip->vbat_low_irq_enabled) {
 			enable_irq(chip->batt_irq[VBATT_LOW].irq);
 			enable_irq_wake(chip->batt_irq[VBATT_LOW].irq);
@@ -3838,8 +3839,9 @@ static void status_change_work(struct work_struct *work)
 		if (capacity == 100)
 			fg_configure_soc(chip);
 	} else if (chip->status == POWER_SUPPLY_STATUS_DISCHARGING) {
-		is_charging = false;
-
+		is_charging = false; 
+                if (chip->vbat_low_irq_enabled &&
+                               !chip->use_vbat_low_empty_soc) {
 		if (chip->vbat_low_irq_enabled) {
 			disable_irq_wake(chip->batt_irq[VBATT_LOW].irq);
 			disable_irq_nosync(chip->batt_irq[VBATT_LOW].irq);
