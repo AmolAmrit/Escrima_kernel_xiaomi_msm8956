6e304b0faf61a90bd60b214dba43ad42444b72f6
AmolAmrit <amol.amrit03@outlook.com>
2017-10-16 23:11:02 +0530

    Add charging_state.h from Agni [WIP]
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

---
+++ include/linux/charging_state.h
@@ -0,0 +1,20 @@
+/*
+ * include/linux/charging_state.h
+ *
+ * Copyright (c) 2017 Parvinder Singh
+ * psndna88@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _LINUX_CHARGING_STATE_H
+#define _LINUX_CHARGING_STATE_H
+
+extern bool charging_detected(void);
+
+// UKSM RUN ON CHARGINGs
+extern void uksm_charging_switcher(void);
+
+#endif
