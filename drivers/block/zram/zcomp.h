ac9b49d85b30fb919465033cd1b2ba13112e9803
AmolAmrit <amol.amrit03@outlook.com>
2019-08-12 10:29:46 +0200

    Revert "zram: make compression algorithm selection possible"
    This reverts commit 2acfd207e1d073436e7c0689229f7297d29c1569.

--- drivers/block/zram/zcomp.h
+++ drivers/block/zram/zcomp.h
@@ -50,8 +50,6 @@ struct zcomp {
 	void (*destroy)(struct zcomp *comp);
 };
 
-ssize_t zcomp_available_show(const char *comp, char *buf);
-
 struct zcomp *zcomp_create(const char *comp, int max_strm);
 void zcomp_destroy(struct zcomp *comp);
 
