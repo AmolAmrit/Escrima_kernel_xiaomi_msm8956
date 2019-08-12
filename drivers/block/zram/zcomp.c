f3097945fcafe8fdcb619c55129067a98442aee9
AmolAmrit <amol.amrit03@outlook.com>
2019-08-12 10:29:42 +0200

    Revert "zram: add lz4 algorithm backend"
    This reverts commit d8709733a9f120f0fcbc9edcb1d0aea47c9dcd2f.

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -15,9 +15,6 @@
 
 #include "zcomp.h"
 #include "zcomp_lzo.h"
-#ifdef CONFIG_ZRAM_LZ4_COMPRESS
-#include "zcomp_lz4.h"
-#endif
 
 /*
  * single zcomp_strm backend
@@ -44,9 +41,6 @@ struct zcomp_strm_multi {
 
 static struct zcomp_backend *backends[] = {
 	&zcomp_lzo,
-#ifdef CONFIG_ZRAM_LZ4_COMPRESS
-	&zcomp_lz4,
-#endif
 	NULL
 };
 
