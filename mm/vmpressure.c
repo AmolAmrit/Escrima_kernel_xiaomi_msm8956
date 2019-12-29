715326d685010d8401c3dd38a6c32e5720649caa
AmolAmrit <amol.amrit03@outlook.com>
2019-12-29 22:05:31 +0530

    Revert "vmpressure: do not check for pending work to prevent from new work"
    This reverts commit 3e173fc7c8d89c62a34f60fe575e1121aaf43252.

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -300,7 +300,7 @@ void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg,
 	scanned = vmpr->scanned;
 	spin_unlock(&vmpr->sr_lock);
 
-	if (scanned < vmpressure_win)
+	if (scanned < vmpressure_win || work_pending(&vmpr->work))
 		return;
 	schedule_work(&vmpr->work);
 }
